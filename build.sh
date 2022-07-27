c8e39a54137c3cbd5c567d9bd8fb3c17faa6a8af
sstringer <steven.stringer@suse.com>
2022-07-27 12:22:38 +0100

    Added sbd prep

--- build.sh
+++ build.sh
@@ -1,42 +1,29 @@
 #!/bin/bash
-
-ErrChk() { 
-  if [[ $? -ne 0 ]] ; then
-    echo "Command failed"  
-    exit 1
-  fi
-}
+set -e
 
 source variables.sh
-ErrChk
 
 AnsFlgs="-i ./terraform/${PROVIDER}/inventory.yaml"
 AnsPlybkPath="./ansible/playbooks"
 
-
 ### TERRAFORM BIT ###
 terraform -chdir=./terraform/${PROVIDER} apply -auto-approve
-ErrChk
 
 ### ANSIBLE BIT ###
-
+# Accept new ssh keys for ansible-controlled hosts
 ansible ${AnsFlgs} all -a true --ssh-extra-args="-l cloudadmin -o UpdateHostKeys=yes -o StrictHostKeyChecking=accept-new"
-ErrChk
 
+# Run registration
 ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/registration.yaml -e "reg_code=${REG_CODE}" -e "email_address=${EMAIL}"
-ErrChk
+
+# Option to quit if we don't want to run all plays
+if [[ $1 == 'skip' ]] ; then
+  echo "Skipping build tasks"
+  exit 0
+fi
 
 ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/sap-hana-preconfigure.yaml  -e "use_sapconf=${SAPCONF}"
-ErrChk
-
-ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/iscsi-server-configuration.yaml
-ErrChk
-
+ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/cluster_sbd_prep.yaml
 ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/sap-hana-stroage.yml 
-ErrChk
-
 ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/sap-hana-download-media.yaml 
-ErrChk
-
 ansible-playbook ${AnsFlgs} ${AnsPlybkPath}/sap-hana-install.yaml 
-ErrChk
