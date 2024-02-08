28295ca94b058ead70f6eefee5fe868ddf7fb246
Michele Pagot <michele.pagot@suse.com>
2024-02-08 01:07:09 +0100

    Move to terraform 1.5.7 and add Dockerfile validation
    Improve the Dockerfile to use Terraform 1.5.7 and Python 3.11.
    Add virtual env in the Dockerfile to fix a bug that prevented pip installation of python packages.
    Add bash script to validate the docker image.
    Add githubaction to build the container image and run the validation
    test.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM opensuse/tumbleweed:latest
 ## AZURE
 # way suggested on https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=zypper
 RUN zypper ref && zypper up -y && \
-    zypper install -y tar gzip unzip curl python310-pip openssh && \
+    zypper install -y tar gzip unzip curl python311-pip openssh && \
     rpm --import https://packages.microsoft.com/keys/microsoft.asc && \
     zypper addrepo --name 'Azure CLI' --check https://packages.microsoft.com/yumrepos/azure-cli azure-cli && \
     zypper install --from azure-cli -y azure-cli && \
@@ -21,13 +21,20 @@ RUN curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud
     echo 'source ~/google-cloud-sdk/path.bash.inc' >> ~/.bashrc
 
 ## Terraform
-RUN curl https://releases.hashicorp.com/terraform/1.3.6/terraform_1.3.6_linux_amd64.zip -o terraform.zip && \
+RUN curl https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip -o terraform.zip && \
     unzip terraform.zip -d /usr/local/bin && \
     terraform -install-autocomplete && \
     rm terraform.zip
 
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3.11 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt
+
+COPY requirements.yml .
+RUN  ansible-galaxy install -r requirements.yml
 
 RUN mkdir /src
 WORKDIR /src
---
+++ tools/image_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+img_name=$1
+cre="${cre:-"podman"}"
+
+#$cre build -f Dockerfile -t "${img_name}"
+
+$cre run "${img_name}" terraform --version | grep 1.5.7 || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
+$cre run -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/azure init || ( echo "ERROR[$?] terraform init does not work for azure" ; exit 1 )
+$cre run -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/aws init || ( echo "ERROR[$?] terraform init does not work for aws" ; exit 1 )
+$cre run  -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/gcp init || ( echo "ERROR[$?] terraform init does not work for google" ; exit 1 )
+
+$cre run "${img_name}" python3.11 --version | grep 3.11 || ( echo "ERROR[$?] wrong or not usable Python" ; exit 1 )
+$cre run "${img_name}" pip3.11 --version || ( echo "ERROR[$?] wrong or not usable pip" ; exit 1 )
+$cre run "${img_name}" pip3.11 freeze | grep ansible-core || ( echo "ERROR[$?] ansible-core not installed" ; exit 1 )
+$cre run "${img_name}" ansible --version || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
+$cre run "${img_name}" ansible-galaxy --version || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
+
+$cre run "${img_name}" pip3.11 freeze | grep aws || ( echo "ERROR[$?] aws cli not installed" ; exit 1 )
+$cre run "${img_name}" aws --version || ( echo "ERROR[$?] wrong or not usable aws" ; exit 1 )
+$cre run "${img_name}" az --version || ( echo "ERROR[$?] wrong or not usable az" ; exit 1 )
+#$cre run "${img_name}" cat /root/.bashrc
+# $cre run "${img_name}" gcloud --version
+
+
