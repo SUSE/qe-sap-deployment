aad75517723aac554f281a24f1943166cf1a1e77
Michele Pagot <michele.pagot@suse.com>
2024-06-13 18:55:13 +0200

    Enable flake8 on the UT code (#241)
    Enable flake8 on the test/unit folder
    Add it to github workflow for PR
    Change flake8 requirements to avoid issue in Python 3.12 due to
    https://github.com/PyCQA/flake8/issues/1845
    Fix some warnings in the test code.

--- scripts/qesap/test/unit/test_qesap_lib_cmd_ansible.py
+++ scripts/qesap/test/unit/test_qesap_lib_cmd_ansible.py
@@ -1,4 +1,3 @@
-from unittest import mock
 import logging
 import os
 
@@ -9,18 +8,29 @@ log = logging.getLogger(__name__)
 
 
 def test_export_ansible_output():
-    '''
+    """
     Utility function that get the ansible command line and the command output.
     Function calculate the log name by extracting the ansible playbook name from the command line.
     Function take the content of out and write it to a file in the current directory
-    
-    '''
+    """
 
     test_dir = os.getcwd()
-    test_file = os.path.join(test_dir, 'ansible.testAll.log.txt')
-    command_to_sent = {'test1':'test_value1', 'test2':'test_value2', 'test3':'test_value3', 'test4':'test_value4',
-                        'cmd':['/tmp/exec_venv/bin/ansible', '-vvvv', '-i', '/root/qe-sap-deployment/terraform/aws/inventory.yaml',
-                              '/some/immaginary/path/testAll.yaml', '-a', '--ssh-extra-args="-l cloudadmin -o UpdateHostKeys=yes -o StrictHostKeyChecking=accept-new"']}
+    test_file = os.path.join(test_dir, "ansible.testAll.log.txt")
+    command_to_sent = {
+        "test1": "test_value1",
+        "test2": "test_value2",
+        "test3": "test_value3",
+        "test4": "test_value4",
+        "cmd": [
+            "/tmp/exec_venv/bin/ansible",
+            "-vvvv",
+            "-i",
+            "/root/qe-sap-deployment/terraform/aws/inventory.yaml",
+            "/some/immaginary/path/testAll.yaml",
+            "-a",
+            '--ssh-extra-args="-l cloudadmin -o UpdateHostKeys=yes -o StrictHostKeyChecking=accept-new"',
+        ],
+    }
     ansible_output = """DEBUG    OUTPUT: ansible [core 2.13.5]
                         DEBUG    OUTPUT:   config file = None
                         DEBUG    OUTPUT:   configured module search path = ['/root/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
