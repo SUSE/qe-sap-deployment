d0eb42537bde8f957cd69b311cdda10e47d94196
Michele Pagot <michele.pagot@suse.com>
2025-01-28 16:57:36 +0100

    Manage terraform AWS IP address and ranges
    Move all the locals about ip address and ranges to a dedicated file.
    Change indentation to split long lines.
    Create intermediate new locals to make algorithm hopefully more readable.
    Start considering 4 initial IP address that AWS reserve.
    Calculate each range and IP dinamically using the previous one as
    starting point.

--- terraform/aws/infrastructure.tf
+++ terraform/aws/infrastructure.tf
@@ -1,3 +1,5 @@
+# Query for VPC id if user does not provide one
+# for an existing VPC created outside of this deployment
 data "aws_vpc" "current-vpc" {
   count = var.vpc_id != "" ? 1 : 0
   id    = var.vpc_id
@@ -17,20 +19,6 @@ locals {
   vpc_id            = var.vpc_id == "" ? aws_vpc.vpc.0.id : var.vpc_id
   internet_gateway  = var.vpc_id == "" ? aws_internet_gateway.igw.0.id : data.aws_internet_gateway.current-gateway.0.internet_gateway_id
   security_group_id = var.security_group_id != "" ? var.security_group_id : aws_security_group.secgroup.0.id
-  vpc_address_range = var.vpc_id == "" ? var.vpc_address_range : (var.vpc_address_range == "" ? data.aws_vpc.current-vpc.0.cidr_block : var.vpc_address_range)
-
-  infra_subnet_address_range = var.infra_subnet_address_range != "" ? var.infra_subnet_address_range : cidrsubnet(local.vpc_address_range, 8, 0)
-
-  hana_subnet_address_range = length(var.hana_subnet_address_range) != 0 ? var.hana_subnet_address_range : [
-  for index in range(var.hana_count) : cidrsubnet(local.vpc_address_range, 8, index + 1)]
-
-  # The 2 is hardcoded because we create 2 subnets for NW always
-  netweaver_subnet_address_range = length(var.netweaver_subnet_address_range) != 0 ? var.netweaver_subnet_address_range : [
-  for index in range(2) : cidrsubnet(local.vpc_address_range, 8, index + var.hana_count + 1)]
-
-  # The 2 is hardcoded considering we create 2 subnets for NW always
-  drbd_subnet_address_range = length(var.drbd_subnet_address_range) != 0 ? var.drbd_subnet_address_range : [
-  for index in range(2) : cidrsubnet(local.vpc_address_range, 8, index + var.hana_count + 2 + 1)]
 }
 
 # AWS key pair
