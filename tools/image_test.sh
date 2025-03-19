714ad919628438d45a98f7be38af589d51e00f25
Michele Pagot <michele.pagot@suse.com>
2025-03-19 16:31:33 +0100

    Update gcloud installer (#331)
    TEAM-10178 change installer binary usr from google, using one without
    version specific. Change the way gcloud is tested by giving full binary
    path: somehow the installation method does not add it to the PATH, maybe
    as we run installation as root user.

--- tools/image_test.sh
+++ tools/image_test.sh
@@ -5,21 +5,27 @@ cre="${cre:-"podman"}"
 
 #$cre build -f Dockerfile -t "${img_name}"
 
+echo "=== Test terraform binary"
 $cre run "${img_name}" terraform --version | grep 1.5.7 || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
 $cre run -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/azure init || ( echo "ERROR[$?] terraform init does not work for azure" ; exit 1 )
 $cre run -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/aws init || ( echo "ERROR[$?] terraform init does not work for aws" ; exit 1 )
 $cre run  -v $(pwd):/src "${img_name}" terraform -chdir=/src/terraform/gcp init || ( echo "ERROR[$?] terraform init does not work for google" ; exit 1 )
 
+echo "=== Test ansible"
 $cre run "${img_name}" python3.11 --version | grep 3.11 || ( echo "ERROR[$?] wrong or not usable Python" ; exit 1 )
 $cre run "${img_name}" pip3.11 --version || ( echo "ERROR[$?] wrong or not usable pip" ; exit 1 )
 $cre run "${img_name}" pip3.11 freeze | grep ansible-core || ( echo "ERROR[$?] ansible-core not installed" ; exit 1 )
 $cre run "${img_name}" ansible --version || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
 $cre run "${img_name}" ansible-galaxy --version || ( echo "ERROR[$?] wrong or not usable Terraform" ; exit 1 )
 
+echo "=== Test awscli"
 $cre run "${img_name}" pip3.11 freeze | grep aws || ( echo "ERROR[$?] aws cli not installed" ; exit 1 )
 $cre run "${img_name}" aws --version || ( echo "ERROR[$?] wrong or not usable aws" ; exit 1 )
-$cre run "${img_name}" az --version || ( echo "ERROR[$?] wrong or not usable az" ; exit 1 )
-#$cre run "${img_name}" cat /root/.bashrc
-# $cre run "${img_name}" gcloud --version
 
+echo "=== Test az"
+$cre run "${img_name}" az --version || ( echo "ERROR[$?] wrong or not usable az" ; exit 1 )
+
+echo "=== Test gcloud"
+$cre run "${img_name}" cat /root/.bashrc
+$cre run "${img_name}" /root/google-cloud-sdk/bin/gcloud --version
 
