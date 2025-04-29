3680f8338079d0308845edfcebdf0d15d34b7b0f
Jan Kohoutek <jkohoutek@suse.com>
2025-04-29 14:13:16 +0200

    AWS disks sizes shrink to minimal requirements

--- terraform/aws/modules/hana_node/variables.tf
+++ terraform/aws/modules/hana_node/variables.tf
@@ -101,37 +101,37 @@ variable "hana_data_disks_configuration" {
   default = [{
     device_name = "/dev/sdb"
     disk_type   = "gp2"
-    disk_size   = 128
+    disk_size   = 40
     },
     {
       device_name = "/dev/sdc"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 40
     },
     {
       device_name = "/dev/sdd"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 16
     },
     {
       device_name = "/dev/sde"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 16
     },
     {
       device_name = "/dev/sdf"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 32
     },
     {
       device_name = "/dev/sdg"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 64
     },
     {
       device_name = "/dev/sdh"
       disk_type   = "gp2"
-      disk_size   = 128
+      disk_size   = 32
   }]
   description = <<EOF
     This list of object describes how the disks will be created for AWS HANA Nodes and is very similar to Azure. Ansible expects seven disks for HANA
